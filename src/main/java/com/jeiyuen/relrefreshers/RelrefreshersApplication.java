f818bacc39c2b24927bfc57ed9c32e24c42ed4aa
gjuanillo <gjuanillo.dev@gmail.com>
2025-04-30 17:09:55 +0800

    Initialize Project

---
+++ src/main/java/com/jeiyuen/relrefreshers/RelrefreshersApplication.java
@@ -0,0 +1,13 @@
+package com.jeiyuen.relrefreshers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelrefreshersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelrefreshersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeiyuen/relrefreshers/RelrefreshersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeiyuen.relrefreshers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelrefreshersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
