8437dd6acca30f02abbc19055b78dcb5f94b811b
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-01 21:55:15 +0800

    User-Group Many-to-Many

---
+++ src/main/java/com/jeiyuen/relrefreshers/models/SocialGroup.java
@@ -0,0 +1,20 @@
+package com.jeiyuen.relrefreshers.models;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class SocialGroup{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(mappedBy = "groups")
+    private Set<SocialUser> socialUsers;
+}
--- src/main/java/com/jeiyuen/relrefreshers/models/SocialUser.java
+++ src/main/java/com/jeiyuen/relrefreshers/models/SocialUser.java
@@ -1,17 +1,22 @@
 package com.jeiyuen.relrefreshers.models;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
 @Entity
-public class SocialUser{
+public class SocialUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +27,8 @@ public class SocialUser{
 
     @OneToMany(mappedBy = "socialUser")
     private List<Post> posts = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "user_group", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "group_id"))
+    private Set<SocialGroup> groups = new HashSet<>();
 }
